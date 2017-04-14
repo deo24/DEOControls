33d8458eb26541d8a0da9c83cb6c59d76f07727f
杨栋 <deo126@126.com>
2017-04-14 17:26:52 +0800

    项目创建

---
+++ DEOImageView/DEOImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DEOImageView
+//
+//  Created by 杨栋 on 2017/4/14.
+//  Copyright © 2017年 杨栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DEOImageView/DEOImageView/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  DEOImageView
+//
+//  Created by 杨栋 on 2017/4/14.
+//  Copyright © 2017年 杨栋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
